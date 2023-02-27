65b253223cd0a96555c6d55ca5b072ee2ac0a9c3
Mohamed Alsabbagh <mohamed.alsabbagh@mhp.com>
2023-02-27 20:53:33 +0100

    make

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'Demo Demo'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.plantuml',
+extensions = [
               'sphinx_needs',
               #'sphinx.ext.autodoc',
               #'sphinx.ext.napoleon',
